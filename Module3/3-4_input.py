ecf18ddbdd1ac629cd5f582c9b48f7d3828ff08c
unknown <a841023jeff@gmail.com>
2025-11-02 16:36:32 +0800

    Module 4

--- Module3/3-4_input.py
+++ Module3/3-4_input.py
@@ -1,9 +1,13 @@
 # input() 函數 讓使用者在終端機輸入資料
 
 # 取得使用者輸入的資料
-
+user_inpiut = input("請輸入數字: ")
+print(user_inpiut)
+print(type(user_inpiut))
 
 
 # 將使用者輸入強制轉型成 int
-
-
+user_inpiut = int(user_inpiut)
+print(type(user_inpiut))
+if user_inpiut > 10:
+    print("num 大於 10")
--- Module3/practice.py
+++ Module3/practice.py
@@ -4,6 +4,25 @@
 # 數字太大 要提示「請輸入更小的數字」
 # 數字太小 要提示「請輸入更大的數字」
 # 使用者猜對要回傳「恭喜中獎」
+import random
+password = random.randint(1, 100)
+maximum = 100
+minimum = 1
+
+guess = int(input(f"Enter a integer from {str(minimum)} ~ {str(maximum)}: "))
+while guess != password:
+    if guess > maximum or guess < minimum:
+        print("超出範圍請重新輸入")
+    elif guess > password:
+        maximum = guess
+        print("請輸入更小的數字")
+    else:
+        minimum = guess
+        print("請輸入更大的數字")
+    guess = int(input(f"Enter a integer from {str(minimum)} ~ {str(maximum)}: "))
+        
+
+print("恭喜中獎")
 
 
 
